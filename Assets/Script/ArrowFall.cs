3ee7086712fb68b4a81bd3c11abf5ba3dc2702cc
UmiKitsunee <Antoniomiguelcdayno@gmail.com>
2024-07-23 19:17:19 +0800

    RhythmGame: Added arrow falling script, Added arrow on the scene as well as empty gameobject as arrows parent

---
+++ Assets/Script/ArrowFall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFall : MonoBehaviour
+{
+
+    public float musicBeat;
+
+    public bool hasStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicBeat /= 60f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }   
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, musicBeat * Time.deltaTime, 0f);
+        }
+    }
+}
